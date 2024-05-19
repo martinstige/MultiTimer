a29942a662e63b2aff044a8d7b13d49024dcf0b8
Martin Stige <martin@stige.net>
2024-05-19 13:18:50 +0200

    Only show hours when hour has value

--- src/time-utils.ts
+++ src/time-utils.ts
@@ -1,15 +1,35 @@
-
 export const getDate = (time: number) => {
-    const date = new Date();
-    date.setTime(time);
-    return date;
-  };
+  const date = new Date();
+  date.setTime(time);
+  return date;
+};
 
-  export const toTimeString = (time: Date) => {
-    const currentHours = (time.getHours() - 1).toString().padStart(2, "0");
-    const currentMinutes = time.getMinutes().toString().padStart(2, "0");
-    const currentSeconds = time.getSeconds().toString().padStart(2, "0");
-    const currentMilliseconds = time.getMilliseconds().toString();
+export const toTimeString = (time: Date, includeHundreads = true) => {
+  const currentHours = (time.getHours() - 1)
+    .toString()
+    .padStart(2, "0")
+    .substring(0, 2);
+  const currentMinutes = time
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")
+    .substring(0, 2);
+  const currentSeconds = time
+    .getSeconds()
+    .toString()
+    .padStart(2, "0")
+    .substring(0, 2);
+  const currentMilliseconds = time
+    .getMilliseconds()
+    .toString()
+    .padStart(2, "0")
+    .substring(0, 2);
 
-    return `${currentHours}:${currentMinutes}:${currentSeconds}`;
-  };
+  const includeHours = time.getHours() > 1;
+
+  return `${
+    includeHours ? currentHours + ":" : ""
+  }${currentMinutes}:${currentSeconds}${
+    includeHundreads ? `.${currentMilliseconds}` : ""
+  }`;
+};
